5be46bef59bf852474f6c9e32b4f35b7bf4f6902
lacomaco <lacomaco122@gmail.com>
2020-10-26 23:11:24 +0900

    [CHORE] [#03] 리액트 환경설정 빠트린 styled-component를 추가하였습니다.

--- frontEnd/src/components/App.js
+++ frontEnd/src/components/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+const Header = styled.h1`
+  background-color: tomato;
+`;
 const ComponentTwo = () => {
   return <div>im Two</div>;
 };
 const App = () => {
   return (
     <div>
-      <h1>Hello, Webpack!! with hhReact</h1>
+      <Header>Hello, Webpack!! with hhReact</Header>
       <h2>
         <ComponentTwo />
       </h2>
