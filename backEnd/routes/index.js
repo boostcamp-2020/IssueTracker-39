e8caaae1c881a38b70e37905f34e80fa30703fe1
lacomaco <lacomaco122@gmail.com>
2020-10-27 14:37:17 +0900

    [REFACTOR] [#18] : react-file은 참조하지 않도록 gitIgnore을 설정하였습니다.
    임시로 public의 index.html을 서빙할 수 있도록 설정하였습니다.

--- backEnd/routes/index.js
+++ backEnd/routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const path = require('path');
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {title: 'Express'});
+  res.sendFile(path.resolve(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
