f36baccbff86fa00c033ccc4569d956384c5b810
liendmch <mon823@naver.com>
2020-10-27 17:04:57 +0900

    [CHORE] [#42]  Sequelize 초기 세팅 & .env 설정
    .env 설정 .env template 추가 Sequelize 설치 및 환경설정

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const sequelize = require('./models/index').sequelize;
+sequelize.sync();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
