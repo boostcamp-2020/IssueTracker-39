854ce608b0e742fe9e1b76461b82bb280bd79f03
liendmch <mon823@naver.com>
2020-10-27 17:09:06 +0900

    [ADD] [#10][#8]  Issue  모델 만들기
    user,milestones belongsTo 관계 설정 user,label belongsToMany 관계 설정 comments hasMany 관계 설정

---
+++ backEnd/models/ISSUES.js
@@ -0,0 +1,57 @@
+const {sequelize} = require('.');
+
+const {Model} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class ISSUES extends Model {
+    static associate(models) {
+      this.belongsTo(models.users, {foreignKey: 'author', targetKey: 'idx'});
+      this.belongsTo(models.milestones, {
+        foreignKey: 'milestoneIdx',
+        targetKey: 'idx',
+      });
+      this.belongsToMany(models.users, {through: 'assignees'});
+      this.belongsToMany(models.labels, {through: 'issueLabel'});
+      this.hasMany(models.comments, {foreignKey: 'issueIdx', sourceKey: 'idx'});
+    }
+  }
+  ISSUES.init(
+    {
+      idx: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        unique: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      context: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      createdTime: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      closedTime: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      status: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'issues',
+      timestamps: false,
+    },
+  );
+  return ISSUES;
+};
