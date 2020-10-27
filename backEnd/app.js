ba7a342e652edd1ca400681f6b9500e96c7aed5a
lacomaco <lacomaco122@gmail.com>
2020-10-27 14:33:34 +0900

    [REFACTOR] [#18] esLint,Prettier 포맷에 맞도록 수정하였습니다.

--- backEnd/app.js
+++ backEnd/app.js
@@ -10,7 +10,7 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
