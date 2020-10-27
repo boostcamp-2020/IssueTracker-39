0ba39fdd51b834c6c14c5e52afdc9dc37d2b4fa1
liendmch <mon823@naver.com>
2020-10-27 17:11:21 +0900

    [ADD] [#9] User 모델 만들기
    issue 와 hasMany 및 belongToMany 관계 comments와  hasMany  관계 설정

---
+++ backEnd/models/USERS.js
@@ -0,0 +1,38 @@
+const {sequelize} = require('.');
+
+const {Model} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class USERS extends Model {
+    static associate(models) {
+      this.hasMany(models.issues, {foreignKey: 'author', sourceKey: 'idx'});
+      this.belongsToMany(models.issues, {through: 'assignees'});
+      this.hasMany(models.comments, {foreignKey: 'author', sourceKey: 'idx'});
+    }
+  }
+  USERS.init(
+    {
+      idx: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        unique: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      userId: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      userPw: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'users',
+    },
+  );
+  return USERS;
+};
