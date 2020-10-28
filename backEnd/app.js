4d43041e044581e14fa3d026c14491f18e3eb98d
lacomaco <lacomaco122@gmail.com>
2020-10-28 15:56:18 +0900

    [ADD] [#46] passport 연결
    - passport와 app.js를 연결하였습니다.

--- backEnd/app.js
+++ backEnd/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const passport = require('./config/passport.config');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -17,6 +18,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/api/auth', authRouter);
