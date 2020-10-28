7eb8c72db0f552f6df99b76279313ee00802ea82
lacomaco <lacomaco122@gmail.com>
2020-10-28 16:03:12 +0900

    [ADD] [#47] [#48] Oauth 기능 추가
    - 유저가 없으면 회원가입 시키는 기능 추가
    - 유저 정보에 맞춰 jwt를 발급하는 로직 추가
    - cookie의 token으로 jwt를 발급하고 /으로 리다이렉트 하는 기능을
      추가하였습니다.

--- backEnd/routes/auth.js
+++ backEnd/routes/auth.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('../config/passport.config');
+const {findUserOrCreate, makeToken} = require('../service/auth');
 
-router.get('/github', (req, res, next) => {});
+router.get(
+  '/github',
+  passport.authenticate('github', {session: false, scope: ['user:email']}),
+);
 
-// 나중에
+router.get(
+  '/github/call',
+  passport.authenticate('github', {session: false, failureRedirect: '/wrong'}),
+  async function (req, res) {
+    console.log('im call back');
+    console.log('user', req.user.emails[0]);
+
+    const user = await findUserOrCreate(req.user.emails[0].value);
+    const token = makeToken(user);
+
+    res.cookie('token', token);
+    res.redirect('/');
+  },
+);
+
 router.post('/login', (req, res, next) => {});
 
 module.exports = router;
