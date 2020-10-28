d81ccb8e2255dc743053b1da01c375058e01b7c5
EarlyHail <hojin5633@gmail.com>
2020-10-28 15:57:50 +0900

    [NEW][#28][#51] jwt 토큰 확인, 로그인 화면 구현
    - 로그인 화면 구현
      - github 로그인 버튼 구현
    - jwt 토큰 확인
      local storage 확인
      cookie 확인
      로그인 안되있을 시 로그인 화면 rendering

--- frontEnd/src/components/App.js
+++ frontEnd/src/components/App.js
@@ -1,20 +1,55 @@
 import React from 'react';
 import styled from 'styled-components';
-const Header = styled.h1`
-  background-color: tomato;
-`;
-const ComponentTwo = () => {
-  return <div>im Two</div>;
+
+import './App.css';
+
+import Login from '../pages/login';
+
+/**
+ * 1. 로컬스토리지 확인
+ *    1.1 토큰이 있을 시 token 리턴
+ * 2. 쿠키 확인
+ *    2.1 쿠키에 있을 시 로컬 스토리지에 저장 후 cookie 삭제 token 리턴
+ * 3. 리턴 토큰 없을 시 로그인 페이지 렌더링
+ * 4. 리턴 토큰 있을 시 리스트 렌더링
+ */
+
+const checkTokenInLocalStorage = () => {
+  const token = localStorage.getItem('token');
+  return token ? token : undefined;
 };
+
+const checkTokenInCookies = () => {
+  try {
+    const token = document.cookie
+      .split(';')
+      .find((cookie) => cookie.includes('token='))
+      .split('=')[1];
+    deleteCookie('token');
+    localStorage.token = token;
+  } catch (e) {
+    return undefined;
+  }
+};
+
+const isTokenExists = () => {
+  const token = checkTokenInLocalStorage();
+  if (!!!token) checkTokenInCookies();
+  return checkTokenInLocalStorage();
+};
+
+const deleteCookie = function (name) {
+  document.cookie = name + '=; expires=Thu, 01 Jan 1999 00:00:10 GMT;';
+};
+
+const AppStyle = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
 const App = () => {
-  return (
-    <div>
-      <Header>Hello, Webpack!! with hhReact</Header>
-      <h2>
-        <ComponentTwo />
-      </h2>
-    </div>
-  );
+  const token = isTokenExists();
+  return <AppStyle>{!!token ? <></> : <Login />}</AppStyle>;
 };
 
 export default App;
--- frontEnd/src/index.js
+++ frontEnd/src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 import App from './components/App.js';
-console.log(App);
-console.log(document.getElementById('app'));
 
 ReactDom.render(<App />, document.getElementById('app'));
