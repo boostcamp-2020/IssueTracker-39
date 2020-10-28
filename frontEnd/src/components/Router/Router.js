b938eea85514a98a60c71baaa289c4f1e15149f4
lacomaco <lacomaco122@gmail.com>
2020-10-28 21:46:43 +0900

    [REFACTOR] [#59] Redirect를 useHistory로 변경
    - redirect를 useHistory로 변경하였습니다.
    - HashRouter 위치를 App으로 변경시켰습니다.

--- frontEnd/src/components/Router/Router.js
+++ frontEnd/src/components/Router/Router.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import {HashRouter, Route, Switch, Redirect} from 'react-router-dom';
+import React, {useEffect} from 'react';
+import {
+  HashRouter,
+  Route,
+  Switch,
+  Redirect,
+  useHistory,
+} from 'react-router-dom';
 
 import Login from '../../pages/login';
 
 const IssueTrackerRouter = ({token}) => {
+  const history = useHistory();
+  useEffect(() => {
+    if (!!token) {
+      history.replace('/');
+    } else {
+      history.replace('/login');
+    }
+  }, []);
   return (
-    <HashRouter>
-      {!!token ? (
-        <Redirect path="*" to="/" />
-      ) : (
-        <Redirect path="*" to="/login" />
-      )}
-      <Switch>
-        <Route path="/" exact={true}>
-          메인!
-        </Route>
-        <Route path="/login" exact={true}>
-          <Login />
-        </Route>
-      </Switch>
-    </HashRouter>
+    <Switch>
+      <Route path="/" exact={true}>
+        메인!
+      </Route>
+      <Route path="/login" exact={true}>
+        <Login />
+      </Route>
+    </Switch>
   );
 };
 export default IssueTrackerRouter;
