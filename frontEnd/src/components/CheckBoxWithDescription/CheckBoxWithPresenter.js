a451b309dbebaa0f064f187db3035f4c3b24bc25
lacomaco <lacomaco122@gmail.com>
2020-10-29 14:56:46 +0900

    [ADD] [#66] ListHeader 구현하기
    - IssueListHeader 레이아웃을 잡았습니다.
    - IssueListHeader checkbox 버튼을 구현했습니다.
    - IssueListHeader 옆의 필터 버튼을 임시로 구현했습니다.

---
+++ frontEnd/src/components/CheckBoxWithDescription/CheckBoxWithPresenter.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const CheckBoxLayout = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const CheckBox = styled.input.attrs({
+  type: 'checkbox',
+})``;
+
+const CheckSelectCount = styled.span`
+  margin-left: 20px;
+  font-size: 20px;
+  color: #858585;
+`;
+const CheckBoxWithPreseter = ({checkBoxClick, selectedCount}) => {
+  console.log(checkBoxClick);
+  console.log(selectedCount);
+  return (
+    <CheckBoxLayout>
+      <CheckBox onClick={checkBoxClick}></CheckBox>
+      <CheckSelectCount>{selectedCount} selected</CheckSelectCount>
+    </CheckBoxLayout>
+  );
+};
+
+export default CheckBoxWithPreseter;
--- frontEnd/src/pages/IssueListPage/IssueMain/IssueList/index.js
+++ frontEnd/src/pages/IssueListPage/IssueMain/IssueList/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import IssueListHeader from './IssueListHeader';
 
 const IssueNavigationLayout = styled.nav`
-  display: flex;
+  width: 100%;
+  background-color: #f7f8fa;
+  padding: 20px 30px;
 `;
 
 const IssueNavigationList = () => {
-  return <IssueNavigationLayout>이슈리스트 입니다</IssueNavigationLayout>;
+  return (
+    <IssueNavigationLayout>
+      <IssueListHeader></IssueListHeader>
+    </IssueNavigationLayout>
+  );
 };
 
 export default IssueNavigationList;
