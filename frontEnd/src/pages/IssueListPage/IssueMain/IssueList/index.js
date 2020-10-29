7e9c62bb4059ce6b703a9e93fb27d157fcae509b
lacomaco <lacomaco122@gmail.com>
2020-10-29 15:34:46 +0900

    [TEST] 작업 공유
    IssueHeader와 Issue를 합치는 작업을 공유하기 위해서 임시로 올리는
    용도입니다.

--- frontEnd/src/pages/IssueListPage/IssueMain/IssueList/index.js
+++ frontEnd/src/pages/IssueListPage/IssueMain/IssueList/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import IssueListHeader from './IssueListHeader';
+import Issue from '../../../../components/Issue/Issue';
 
 const IssueNavigationLayout = styled.nav`
   width: 100%;
@@ -8,10 +9,32 @@ const IssueNavigationLayout = styled.nav`
   padding: 20px 30px;
 `;
 
+const IssueListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const dummyIssue = {
+  title: 'This is title',
+  labelTitle: 'FE',
+  labelColor: 'red',
+  createdTime: 'createdTime',
+  closedTime: 'closedTime',
+  status: 'status',
+  author: 'someone',
+  milestoneIdx: '1',
+};
+
 const IssueNavigationList = () => {
   return (
     <IssueNavigationLayout>
       <IssueListHeader></IssueListHeader>
+      <IssueListWrapper>
+        <Issue {...dummyIssue} />
+        <Issue {...dummyIssue} />
+        <Issue {...dummyIssue} />
+      </IssueListWrapper>
     </IssueNavigationLayout>
   );
 };
