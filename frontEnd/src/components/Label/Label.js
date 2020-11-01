08f80e87c851db96c00176b741680cf2d448514b
yyjjjj <lee.yeonjeong.lee@gmail.com>
2020-11-02 03:25:52 +0900

    [REFACTOR] Issue 컴포넌트 리팩토링 및 이미지 추가
    - Issue Component 의 return 되는 형태만 참고하더라도 컴포넌트 구조
    파악에 도움이 되고자 리팩토링
    - milestone, open image에 해당하는 파일 추가

--- frontEnd/src/components/Label/Label.js
+++ frontEnd/src/components/Label/Label.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const LabelStyle = styled.div`
+const LabelStyle = styled.span`
   box-sizing: border-box;
   height: 1rem;
-  padding: 0.2rem;
-  background-color: ${(props) => props.labelColor || "gray"};
+  padding: 0.1em 0.3em;
+  background-color: ${(props) => props.labelColor || 'gray'};
   color: white;
   font-size: 0.8rem;
   border-radius: 3px;
 `;
 
 const Label = ({labelTitle, labelColor}) => {
-    return (
-    <LabelStyle labelColor={labelColor}>
-        {labelTitle}
-    </LabelStyle>
-    );
+  return <LabelStyle labelColor={labelColor}>{labelTitle}</LabelStyle>;
 };
 
 export default Label;
