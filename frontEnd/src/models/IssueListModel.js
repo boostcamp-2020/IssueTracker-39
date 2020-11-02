d9b133504ac2900a394ca8d696a5824ca0361de4
EarlyHail <hojin5633@gmail.com>
2020-11-02 18:19:50 +0900

    [COMBINE][#95] 이슈 리스트 서버 API 이용 렌더링
    - axios를 이용하여 이슈 리스트를 받아온 뒤 렌더링
    
    ### 생긴 문제점들
    - 로그인 후 토큰 삭제 시, 메인 페이지 - 리스트 페이지 - 요청 - 실패 - 메인 페이지 루프가 반복됨
    - 해결 방법 : 메인 페이지의 home 경로를 `"/"` --> `"/home"`으로 변경

--- frontEnd/src/models/IssueListModel.js
+++ frontEnd/src/models/IssueListModel.js
@@ -1,38 +1,84 @@
 /*
 [
-  {
-    idx: 1,
-    author: Camper1,
-    title: 'title1',
-    createdTime: '2020-10-27T09:52:39.000Z',
-    closedTime: null,
-    status: true,
-    milestoneIdx: frontEnd,
-    isCheckBoxCheckd:boolean, //default False
-  },
-  {
-    idx: 2,
-    author: Camper1,
-    title: 'title2',
-    createdTime: '2020-10-27T09:52:39.000Z',
-    closedTime: null,
-    status: true,
-    milestoneIdx: frontEnd,
-    isCheckBoxCheckd:boolean, //default False
-  },
+    {
+        "idx": 1,
+        "author": 1,
+        "title": "title1",
+        "createdTime": "2020-10-27T09:52:39.000Z",
+        "closedTime": null,
+        "status": true,
+        "milestone": {
+            "title": "milestone1"
+        },
+        "labels": [
+            {
+                "title": "backend",
+                "color": "green",
+                "issueLabel": {
+                    "issueIdx": 1,
+                    "labelIdx": 1
+                }
+            },
+            {
+                "title": "frontend",
+                "color": "blue",
+                "issueLabel": {
+                    "issueIdx": 1,
+                    "labelIdx": 2
+                }
+            },
+            {
+                "title": "important",
+                "color": "red",
+                "issueLabel": {
+                    "issueIdx": 1,
+                    "labelIdx": 3
+                }
+            },
+            {
+                "title": "error",
+                "color": "lightred",
+                "issueLabel": {
+                    "issueIdx": 1,
+                    "labelIdx": 4
+                }
+            }
+        ]
+    },
+    ...
 ]
 */
 
 import React, {createContext, useReducer, useEffect} from 'react';
 import issueListDummy from './IssueListDummy';
 import * as _ from 'lodash';
-
+import axiosMaker from '~/*/utils/axios/axiosMaker';
 export const IssueListModelContext = createContext();
 
 export const IssueListInitialize = 'IssueListInitialize';
 export const IssueCheckToggle = 'IssueCheckToggle';
 export const IssueCheckAll = 'IssueCheckAll';
 export const IssueUnCheckAll = 'IssueUnCheckAll';
+import {isTokenExists} from '~/*/components/app/App.js';
+
+export function IssueToggleAction(id) {
+  return {
+    type: IssueCheckToggle,
+    id,
+  };
+}
+
+export function IssueCheckAllAction(id) {
+  return {
+    type: IssueCheckAll,
+  };
+}
+
+export function IssueUnCheckAllAction() {
+  return {
+    type: IssueUnCheckAll,
+  };
+}
 
 export function reducer(state, action) {
   switch (action.type) {
@@ -67,32 +113,18 @@ export function reducer(state, action) {
   }
 }
 
-export function IssueToggleAction(id) {
-  return {
-    type: IssueCheckToggle,
-    id,
-  };
-}
-
-export function IssueCheckAllAction(id) {
-  return {
-    type: IssueCheckAll,
-  };
-}
-
-export function IssueUnCheckAllAction() {
-  return {
-    type: IssueUnCheckAll,
-  };
-}
+const callAxios = () => {
+  return axiosMaker().get('/api/issue/list');
+};
 
 const IssueListModelConsumer = ({children}) => {
   const [store, dispatch] = useReducer(reducer, []);
   useEffect(() => {
-    const data = issueListDummy();
-    dispatch({
-      type: IssueListInitialize,
-      data: issueListDummy(),
+    callAxios().then(({data}) => {
+      dispatch({
+        type: IssueListInitialize,
+        data,
+      });
     });
   }, [dispatch]);
 
