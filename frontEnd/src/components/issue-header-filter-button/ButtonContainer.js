b1cfde74e094da574c3a12e8f75342062a433f1d
lacomaco <lacomaco122@gmail.com>
2020-11-03 00:49:20 +0900

    ADD [#102] Filter Header 버튼내 DropDown 요소에 대한 테스트코드 작성 - FilterHeader 테스트 코드 작성 - FilterHeader의 버튼은 나중에 contextAPI로 렌더링할 데이터를 직접   접근해 가져오게 됩니다. - contextAPI로 전환하면 잊지 않고 테스트 코드도 같이 수정할것

--- frontEnd/src/components/issue-header-filter-button/ButtonContainer.js
+++ frontEnd/src/components/issue-header-filter-button/ButtonContainer.js
@@ -1,6 +1,7 @@
 import React, {useState, useRef, useEffect} from 'react';
 import clickOutSide from '~/*/utils/custom-hooks/clickOutSide';
 import IssueHeaderButtonPresenter from './ButtonPresenter';
+
 const IssueHeaderButtonContainer = ({name}) => {
   const [isOpen, setIsOpen] = useState(false);
   const buttonRef = useRef();
