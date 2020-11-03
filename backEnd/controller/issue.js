af959da2192472df888c27a5436b5cf5578e6117
EarlyHail <hojin5633@gmail.com>
2020-11-03 14:19:26 +0900

    [ADD][#113] author, label, milestone, assignee 필터 API
    - author, label, milestone, assginee 필터 API 생성
       - issue list api의 함수와 같이 사용
          - 필터값이 있을 경우 필터링된 리스트를 반환
          - 필터값이 없을 경우 필터링되지 않은 리스트 (전체 리스트)를 반환
    
    - author, assignee table을 구분하기 위해 models에 as로 별칭 부여

--- backEnd/controller/issue.js
+++ backEnd/controller/issue.js
@@ -1,17 +1,34 @@
 const issueService = require('../service/issue');
 
-const getIssueList = async (req, res, next) => {
-  const issueListResult = await issueService.getIssueList();
-  if (!!!issueListResult) {
-    res.status(400).send();
-    return;
-  }
+// const getIssueList = async (req, res, next) => {
+//   const issueListResult = await issueService.getIssueList();
+//   if (!!!issueListResult) {
+//     res.status(400).send();
+//     return;
+//   }
 
-  const issueList = [];
-  Object.values(issueListResult).forEach(({dataValues: issue}) => {
-    issueList.push(issue);
+//   const issueList = [];
+//   Object.values(issueListResult).forEach(({dataValues: issue}) => {
+//     issueList.push(issue);
+//   });
+//   res.json(issueList);
+// };
+
+/**
+ * author: integer (idx)
+ * label: integer (idx)
+ * milestone: integer (idx)
+ * assignee: integer (idx)
+ */
+const getIssueList = async (req, res, next) => {
+  const {author, label, milestone, assignee} = req.body;
+  const issueListResult = await issueService.getIssueList({
+    author,
+    label,
+    milestone,
+    assignee,
   });
-  res.json(issueList);
+  res.json(issueListResult);
 };
 
 module.exports = {
