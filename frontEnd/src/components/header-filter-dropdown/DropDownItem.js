d5b4891a1032ab00fa6b4e129e73107c2ed5a6df
EarlyHail <hojin5633@gmail.com>
2020-11-05 19:15:14 +0900

    [FIX] Dropdown 클릭 시 색상이 렌더링 되지 않는 오류 수정
    - Dropdown 클릭 후 Filter Input에 값이 들어간 뒤 Dropdown Item에 Background가 변경되지 않는 것 수정

--- frontEnd/src/components/header-filter-dropdown/DropDownItem.js
+++ frontEnd/src/components/header-filter-dropdown/DropDownItem.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import style from 'styled-components';
 import ColorBoxItem from './ColorBoxItem';
+
 const ContentWrapper = style.li`
 padding:10px;
+background-color: ${(props) => (props.selected ? '#DAFFFF' : 'white')};
+font-weight: ${(props) => (props.selected ? 'bold' : '')};
 `;
 const ContentTitle = style.p`
 font-size:15px;
@@ -18,9 +21,10 @@ const ContentDescription = style.p`
 font-size:13px;
 color:#A3A7AC;
 `;
-const DropDownItem = ({title, description, color, onClick}) => {
+
+const DropDownItem = ({title, description, color, onClick, selected}) => {
   return (
-    <ContentWrapper onClick={() => onClick(title)}>
+    <ContentWrapper onClick={() => onClick(title)} selected={selected}>
       <TitleWrapper>
         {color ? <ColorBoxItem color={color} /> : null}
         <ContentTitle>{title}</ContentTitle>
