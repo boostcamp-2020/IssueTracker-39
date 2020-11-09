87423d70e3b63bce41b02d6a33cd94a9fd9e2742
lacomaco <lacomaco122@gmail.com>
2020-11-09 18:20:45 +0900

    [ADD] [#152] MileStone List 윤곽, 레이아웃 구현
    - MileStoneList의 윤곽부분과 레이아웃 마크업 작업을 수행했습니다.

--- frontEnd/src/pages/milestone/milestone-body/MileStoneBody.js
+++ frontEnd/src/pages/milestone/milestone-body/MileStoneBody.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import MileStoneHeader from '../milestone-header/MileStoneHeader';
+import MileStoneList from '../milestone-list/MileStoneList';
+
+const HeaderWrapper = styled.div`
+
+margin-bottom:50px;
+`;
 
 const mileStoneWidth = '90%';
 const Body = styled.main`
@@ -9,9 +15,14 @@ const Body = styled.main`
   margin: 5% auto;
 `;
 const MileStoneBody = () => {
-  return <Body>
-    <MileStoneHeader/>
-  </Body>;
+  return (
+    <Body>
+      <HeaderWrapper>
+        <MileStoneHeader />
+      </HeaderWrapper>
+      <MileStoneList />
+    </Body>
+  );
 };
 
 export default MileStoneBody;
