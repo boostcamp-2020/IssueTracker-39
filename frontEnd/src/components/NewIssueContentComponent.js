ee02521799166adb93dcc6c5de976a8679595b63
yyjjjj <lee.yeonjeong.lee@gmail.com>
2020-11-09 17:05:23 +0900

    [ADD] [#150] 이슈생성 페이지 Form 부분 view 추가
    - 이슈생성 페이지 Form 부분 view 추가
    - component 폴더에 depth 없이 작업

---
+++ frontEnd/src/components/NewIssueContentComponent.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+const SectionWriteTitle = styled.div`
+  margin: 0 8px;
+  padding: 8px 0;
+  width: 4em;
+  transform: translateY(1px);
+  text-align: center;
+  border: 1px solid lightgray;
+  border-bottom: 1px solid white;
+  border-radius: 5px 5px 0 0;
+`;
+
+const NewIssueContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid lightgray;
+  border-width: 1px 0 0 0;
+`;
+
+const NewIssueContent = styled.textarea`
+  height: 200px;
+  min-height: 200px;
+  padding: 8px;
+  margin: 8px 8px 0 8px;
+  border: 1px solid lightgray;
+  border-bottom: 1px dotted lightgray;
+  resize: vertical;
+`;
+
+export {SectionWriteTitle, NewIssueContent, NewIssueContentWrapper};
---
+++ frontEnd/src/components/NewIssueTitle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NewIssueTitle = styled.input`
+  margin: 8px;
+  padding: 8px;
+  border: 1px solid lightgray;
+  border-radius: 2px;
+  font-size: 1em;
+`;
+
+export default NewIssueTitle;
