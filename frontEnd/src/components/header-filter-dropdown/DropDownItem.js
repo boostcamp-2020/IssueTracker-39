33f2311b4c196a6d4d6f7b035ba9e4c9cfd4a72b
yyjjjj <lee.yeonjeong.lee@gmail.com>
2020-11-10 15:45:35 +0900

    [FIX] typo 수정
    - style -> styled 오타 수정

--- frontEnd/src/components/header-filter-dropdown/DropDownItem.js
+++ frontEnd/src/components/header-filter-dropdown/DropDownItem.js
@@ -1,25 +1,25 @@
 import React, {useContext} from 'react';
-import style from 'styled-components';
+import styled from 'styled-components';
 import ColorBoxItem from './ColorBoxItem';
 
-const ContentWrapper = style.li`
-padding:10px;
-background-color: ${(props) => (props.selected ? '#DAFFFF' : 'white')};
-font-weight: ${(props) => (props.selected ? 'bold' : '')};
+const ContentWrapper = styled.li`
+  padding: 10px;
+  background-color: ${(props) => (props.selected ? '#DAFFFF' : 'white')};
+  font-weight: ${(props) => (props.selected ? 'bold' : '')};
 `;
-const ContentTitle = style.p`
-font-size:15px;
-color:#474747;
-margin-bottom:5px;
+const ContentTitle = styled.p`
+  font-size: 15px;
+  color: #474747;
+  margin-bottom: 5px;
 `;
-const TitleWrapper = style.div`
-display:flex;
-flex-direction:row;
-align-items:center;
+const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 `;
-const ContentDescription = style.p`
-font-size:13px;
-color:#A3A7AC;
+const ContentDescription = styled.p`
+  font-size: 13px;
+  color: #a3a7ac;
 `;
 
 const DropDownItem = ({title, description, color, onClick, selected}) => {
