86cb480c042d098553da36c6ecb3c9ac40662c72
lacomaco <lacomaco122@gmail.com>
2020-11-10 21:50:35 +0900

    [ADD] [#168] Form값 Validation getter setter 추가
    - id값은 조금 특별한 값이여서 getter setter 기능을 더해 예외 처리를 추가하였습니다.

--- backEnd/validation/milestoneFormValid.js
+++ backEnd/validation/milestoneFormValid.js
@@ -1,5 +1,5 @@
 class MilestoneFormVO {
-  constructor(title, dueDate, description) {
+  constructor(title, dueDate, description, id) {
     if (dueDate !== undefined) {
       this.validDate(dueDate);
       this.dueDate = new Date(dueDate);
@@ -9,6 +9,7 @@ class MilestoneFormVO {
 
     this.description = description ? description : '';
     this.title = title ? title : '';
+    this._id = id;
   }
 
   validDate(date) {
@@ -17,6 +18,24 @@ class MilestoneFormVO {
     if (isNaN(validDate)) {
       throw new Error('옳바르지 않은 날짜 형식');
     }
+  }
+
+  set id(id){
+    if (id === undefined) {
+      this._id= -1;
+    }
+    if (typeof id !== 'number') {
+      throw new Error('milestone id값은 항상 정수여야합니다.');
+    }
+    this._id = id;
+
+  }
+
+  get id() {
+    if (this._id === -1) {
+      throw new Error('id값이 들어오지 않았습니다.');
+    }
+    return this._id;
   }
 }
 
