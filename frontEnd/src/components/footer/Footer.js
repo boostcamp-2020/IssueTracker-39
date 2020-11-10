e9bf031c8060eda8919f9d4f1194542f99ae1f42
EarlyHail <hojin5633@gmail.com>
2020-11-10 16:28:26 +0900

    [FIX][#166] Footer style 수정
    - 흰색 배경으로, body의 바로 아래 나오도록 수정
    - 기타 css 수정

--- frontEnd/src/components/footer/Footer.js
+++ frontEnd/src/components/footer/Footer.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const textColor = 'gray';
 const FooterStyle = styled.div`
   box-sizing: border-box;
-  background-color: #242a2e;
   font-size: 15px;
-  color: white;
-  display: flex;
-  flex-direction: column;
-  padding: 30px 50px;
+  margin: 0 5%;
+  border-top: 1px solid lightgray;
+  padding-top: 25px;
+  color: ${textColor};
   a {
     text-decoration: none;
     margin-left: 5px;
+    color: ${textColor};
     &:visited {
       text-decoration: none;
-      color: white;
+      color: ${textColor};
     }
+    &:hover {
+      color: blue;
+      text-decoration: underline;
+    }
+  }
+  ul {
+    display: flex;
+    flex-direction: row;
   }
   li {
     margin-top: 10px;
+    margin-right: 35px;
   }
 `;
 
@@ -32,16 +42,16 @@ const Footer = () => {
       <CopyRight>&copy;2020 BoostCamp, Group 39, All rights reserved</CopyRight>
       <ul>
         <li>
-          J045 김영근 <a href="https://github.com/lacomaco">@lacomaco</a>
+          J045-<a href="https://github.com/lacomaco">@lacomaco</a>
         </li>
         <li>
-          J068 문석암 <a href="https://github.com/mon823">@mon823</a>
+          J068-<a href="https://github.com/mon823">@mon823</a>
         </li>
         <li>
-          J151 이연정 <a href="https://github.com/yyjjjj">@yyjjjj</a>
+          J151-<a href="https://github.com/yyjjjj">@yyjjjj</a>
         </li>
         <li>
-          J168 이호진 <a href="https://github.com/EarlyHail">@EarlyHail</a>
+          J168-<a href="https://github.com/EarlyHail">@EarlyHail</a>
         </li>
       </ul>
     </FooterStyle>
