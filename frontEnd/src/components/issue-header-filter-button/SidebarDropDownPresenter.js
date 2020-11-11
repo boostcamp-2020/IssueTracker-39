ba6ff0e3cb6415b0ddc07cb0f9f99147f491b026
yyjjjj <lee.yeonjeong.lee@gmail.com>
2020-11-11 18:51:16 +0900

    [ADD] [#150] 조건에 따른 렌더링 변화(dropdown 프레임)
    - dropdown 프레임 재활용을 위해 조건에 따른 렌더링 변화

---
+++ frontEnd/src/components/issue-header-filter-button/SidebarDropDownPresenter.js
@@ -0,0 +1,83 @@
+import React, {useContext} from 'react';
+import styled, {keyframes} from 'styled-components';
+import {SidebarModelContext} from '~/*/models/SidebarModel';
+import DropDownItem from './SidebarDropDownItem';
+import {modelStore} from '~/*/models/store';
+
+const Wrapper = styled.ul`
+  position: absolute;
+  bottom: 0%;
+  left: 0%;
+  transform: translateY(100%);
+  box-shadow: 2px 2px 2px lightgray;
+  border: 1px solid lightgray;
+  background-color: #ffffff;
+  /* @리팩토링 필요 */
+  z-index: 10;
+`;
+
+const DropDownHeader = styled.li`
+  background-color: #f7f8fa;
+  font-size: 16px;
+  font-weight: bold;
+  padding: 5px;
+`;
+
+//description_title
+const SidebarDropDown = ({dropDownName}) => {
+  const {
+    labels,
+    onUpdateLables,
+    milestone,
+    onUpdateMilestone,
+    assignees,
+    onUpdateAssignees,
+  } = useContext(SidebarModelContext);
+
+  const onClick = (parentName, data) => {
+    if (parentName === 'Assignee') {
+      onUpdateAssignees(data);
+    } else if (parentName === 'Label') {
+      onUpdateLables(data);
+    } else if (parentName === 'Milestone') {
+      onUpdateMilestone(data);
+    }
+
+    console.log(labels);
+    console.log(milestone);
+    console.log(assignees);
+  };
+
+  const checkSelected = (parentName, idx) => {
+    if (parentName === 'Assignee') {
+      return assignees.includes(idx);
+    } else if (parentName === 'Label') {
+      return labels.includes(idx);
+    } else if (parentName === 'Milestone') {
+      return milestone === idx;
+    }
+  };
+
+  const {store, getDropDownItem} = useContext(modelStore[dropDownName]);
+  const dropDownItems = getDropDownItem(store);
+  return (
+    <Wrapper>
+      <DropDownHeader>Filter by {dropDownName}</DropDownHeader>
+      {dropDownItems.map((item, id) => (
+        <DropDownItem
+          onClick={onClick}
+          title={item.title}
+          idx={item.idx}
+          key={id}
+          description={item.description}
+          color={item.color}
+          parentName={dropDownName}
+          selected={checkSelected(dropDownName, item.idx)}
+          // 여기서 선택된거 바꿔줘야해요
+        />
+      ))}
+    </Wrapper>
+  );
+};
+
+export default SidebarDropDown;
