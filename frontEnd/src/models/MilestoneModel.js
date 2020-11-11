f7bdfb1dca64ecedd3a60e937fa79833e8b55408
lacomaco <lacomaco122@gmail.com>
2020-11-11 16:37:45 +0900

    [ADD] [#180] MilestoneModel reducer 추가
    - CRUD에 해당하는 reducer 기능 추가
    - 서버에 CRUD를 보내는 함수 생성

--- frontEnd/src/models/MilestoneModel.js
+++ frontEnd/src/models/MilestoneModel.js
@@ -4,12 +4,52 @@ import axiosMaker from '~/*/utils/axios/axiosMaker';
 const MilestoneInitialize = 'MilestoneInitialize';
 export const MilestoneModelContext = createContext();
 
+const CLOSE_MILESTONE = 'CLOSE_MILESTONE';
+const DELETE_MILESTONE = 'DELETE_MILESTONE';
+const CREATE_MILESTONE = 'CREATE_MILESTONE';
+const UPDATE_MILESTONE = 'UPDATE_MILESTONE';
+
+function stateImmutableCreator(state) {
+  const newArray = [];
+  state.forEach((milestone) => {
+    newArray.push({
+      ...milestone,
+    });
+  });
+  return newArray;
+}
+
 //getDropDownItem
 
 function reducer(state, action) {
   switch (action.type) {
     case MilestoneInitialize: {
       return action.data;
+    }
+    case CLOSE_MILESTONE: {
+      state.find((milestone) => milestone.idx === action.id).opened = false;
+      return stateImmutableCreator(state);
+    }
+
+    case DELETE_MILESTONE: {
+      return state.filter((milestone) => milestone.idx !== action.id);
+    }
+    case CREATE_MILESTONE: {
+      action.milestone.openedIssues = 0;
+      action.milestone.closedIssues = 0;
+      state.push(action.milestone);
+      return stateImmutableCreator(state);
+    }
+    case UPDATE_MILESTONE: {
+      const targetMilestone = state.find(
+        (milestone) => milestone.idx === action.id,
+      );
+
+      targetMilestone.title = action.title;
+      targetMilestone.dueDate = new Date(action.date).toISOString();
+      targetMilestone.description = action.description;
+
+      return stateImmutableCreator(state);
     }
     default:
       throw new Error('Error MilestoneModel');
@@ -38,12 +78,78 @@ const MilestoneModelConsumer = ({children}) => {
     });
   }, [dispatch]);
 
+  const requestCloseMilestone = async (id) => {
+    const axiosInstance = axiosMaker();
+    const result = await axiosInstance.put('/api/milestone/close', {
+      id,
+    });
+    if (result.status === 200) {
+      dispatch({
+        type: CLOSE_MILESTONE,
+        id,
+      });
+    }
+  };
+
+  const requestDeleteMilestone = async (id) => {
+    const axiosInstance = axiosMaker();
+    const result = await axiosInstance.delete(`/api/milestone/${id}`);
+    if (result.status === 200) {
+      dispatch({
+        type: DELETE_MILESTONE,
+        id,
+      });
+    }
+  };
+
+  const requestCreateMilestone = async (title, date, description) => {
+    const axiosInstance = axiosMaker();
+    const result = await axiosInstance.post('/api/milestone', {
+      title,
+      date,
+      description,
+    });
+    if (result.status === 200) {
+      dispatch({
+        type: CREATE_MILESTONE,
+        milestone: result.data.newMilestone,
+      });
+    }
+  };
+
+  //Update Milestone
+  const requestUpdateMilestone = async (id, title, date, description) => {
+    const axiosInstance = axiosMaker();
+    const result = await axiosInstance.put(`/api/milestone/${id}`, {
+      title,
+      date,
+      description,
+    });
+    if (result.status === 200) {
+      dispatch({
+        type: UPDATE_MILESTONE,
+        id,
+        title,
+        date,
+        description,
+      });
+    }
+  };
+
+  const requests = {
+    requestCloseMilestone,
+    requestDeleteMilestone,
+    requestCreateMilestone,
+    requestUpdateMilestone,
+  };
+
   return (
     <MilestoneModelContext.Provider
       value={{
         store,
         getDropDownItem,
         dispatch,
+        requests,
       }}
     >
       {children}
