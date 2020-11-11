cb9c5957d29bf8d15ceb0086da7e8cb9600cd6d8
lacomaco <lacomaco122@gmail.com>
2020-11-11 23:23:59 +0900

    [ADD] [#184] state,이미지 서버 연결
    - CreateNewIssueForm의 컨텐츠를 저장하는 issueContent State를
      추가하였습니다.
    
    - 이미지를 image API에 전송하는 requestImageUpload 함수를
      생성하였습니다.
      성공적으로 이미지에 업로드 하면  생성된 이미지이름을 content state에
      덧붙여씁니다.

--- frontEnd/src/models/TextareaModel.js
+++ frontEnd/src/models/TextareaModel.js
@@ -1,15 +1,37 @@
 import React, {createContext, useEffect, useState} from 'react';
+import axiosMaker from '~/*/utils/axios/axiosMaker';
 
 export const TextareaModelContext = createContext();
-
 const durationTime = 2000;
 
 const TextareaModelConsumer = ({children}) => {
   const [counter, setCounter] = useState();
   const [visibility, setVisibility] = useState('hidden');
+  const [issueContent, setIssueContent] = useState('');
 
   const setCounterWithTextareaLength = (e) => {
     setCounter(e.target.value.length);
+  };
+
+  const issueContentChange = (e) => {
+    setIssueContent(e.target.value);
+  };
+
+  const requestImageUpload = async (formData) => {
+    const axiosInstance = axiosMaker();
+    const result = await axiosInstance.post('/api/issue/image', formData, {
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
+    });
+
+    if (result.status === 200) {
+      setIssueContent((content) => content + result.data.filename);
+    }
+  };
+
+  const requests = {
+    requestImageUpload,
   };
 
   useEffect(() => {
@@ -25,7 +47,14 @@ const TextareaModelConsumer = ({children}) => {
 
   return (
     <TextareaModelContext.Provider
-      value={{setCounterWithTextareaLength, visibility, counter}}
+      value={{
+        setCounterWithTextareaLength,
+        visibility,
+        counter,
+        issueContentChange,
+        issueContent,
+        requests,
+      }}
     >
       {children}
     </TextareaModelContext.Provider>
