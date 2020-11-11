7a4c69f7dd00a94084733a8a417d10486ecd7a1e
lacomaco <lacomaco122@gmail.com>
2020-11-11 16:35:07 +0900

    [REFACTOR] CommonButton 리팩토링
    - COmmonButton에 onClick 이벤트를 넣을 수 있도록 수정하였습니다.

--- frontEnd/src/components/common-button/CommonButton.js
+++ frontEnd/src/components/common-button/CommonButton.js
@@ -7,13 +7,24 @@ const Button = styled.button`
   border-radius: 3px;
   border: 1px solid lightgray;
   padding: 8px 14px;
-  font-size:${(props) => props.fontSize};
-  cursor:pointer;
+  font-size: ${(props) => props.fontSize};
+  cursor: pointer;
 `;
 
-const CommonButton = ({color, textColor, children, fontSize = '15px'}) => {
+const CommonButton = ({
+  color,
+  textColor,
+  children,
+  fontSize = '15px',
+  onClick,
+}) => {
   return (
-    <Button color={color} textColor={textColor} fontSize={fontSize}>
+    <Button
+      onClick={onClick}
+      color={color}
+      textColor={textColor}
+      fontSize={fontSize}
+    >
       {children}
     </Button>
   );
