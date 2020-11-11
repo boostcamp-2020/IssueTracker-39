2f4421a46d85b92e4d66dc9440674cca42d2c77b
lacomaco <lacomaco122@gmail.com>
2020-11-11 20:44:35 +0900

    [ADD] [#183] 글자속에서 이미지 형식 찾기
    - splitTextAndImageInText 함수는 글자속에서 이미지 마크다운 이미지 형식
      [! ... ] 을 찾아내 해당 이미지 소스를 JSX 이미지 태그로 바꿔 배열로
      변환하는 함수입니다. 리액트에서는 배열을 렌더링하는것도 가능해서 이슈
      본문을 이 함수를 사용해서 치환하면 사용 가능할것 같아요.

---
+++ frontEnd/src/utils/splitTextAndImageInText.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Image = styled.image`
+  width: 100px;
+`;
+
+function textImageToJsxTag(images) {
+  return images.map((text) => {
+    const imageSrc = text.replace(/[\[!\]]/g, '');
+    return <Image src={`images/${imageSrc}`}></Image>;
+  });
+}
+
+function isExists(array) {
+  return array.length > 0;
+}
+
+function moveFirstElement(array, target) {
+  if (isExists(array)) {
+    target.push(array.shift());
+  }
+}
+
+function combineTextAndImageTag(text, image) {
+  const result = [];
+  while (text.length > 0 || image.length > 0) {
+    moveFirstElement(text, result);
+    moveFirstElement(image, result);
+  }
+  return result;
+}
+
+export default function splitTextAndImageInText(Text) {
+  const reg = /\[![^\]]+\]/g;
+  const text = Text.split(reg);
+  const image = textImageToJsxTag(Text.match(reg));
+  return combineTextAndImageTag(text, image);
+}
